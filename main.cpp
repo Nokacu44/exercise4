a40855bf9f088b1c94203a5107bb6d28e308103e
SimDeveloper-cripto <catapano.smn.2001@gmail.com>
2022-05-16 12:36:46 +0200

    Effettuare il test daiiiiiiiiiiii

--- main.cpp
+++ main.cpp
@@ -1,16 +1,28 @@
-
 #include "zlasdtest/test.hpp"
-
 #include "zmytest/test.hpp"
-
-/* ************************************************************************** */
-
 #include <iostream>
 
-/* ************************************************************************** */
+using namespace std;
 
 int main() {
-  std::cout << "Lasd Libraries 2022" << std::endl;
-  lasdtest(); // To call in the menu of your library test!
+  ushort scelta = 0;
+
+  cout << "Lasd Libraries 2022" << endl;
+  cout << "[1] PER EFFETTUARE LASDTEST." << endl;
+  cout << "[2] PER VISUALIZZARE LO SVOGLIMENTO DEL QUARTO ESERCIZIO." << endl;
+  cout << "SCELTA: "; 
+  cin >> scelta;
+
+  switch(scelta) {
+    case 1:
+      lasdtest();
+      break;
+    case 2:
+      startTest();
+      break;
+    default:
+      cout << "ERRORE: SCELTA NON DISPONIBILE!" << endl;
+  }
+
   return 0;
 }
